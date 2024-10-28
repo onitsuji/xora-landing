8e8025109a7b0f09c8bcd1d7508fa1cc5ab59b9a
justin-0 <itsjustinoconnell@gmail.com>
2024-10-28 13:19:49 +0000

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+Recreating Figma design from: https://ui8.net/ui8/products/xora---saas-landing-page-kit
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import "./index.css";
+
+export default function App() {
+  return <h1 className="text-6xl text-red-600">XORA</h1>;
+}
