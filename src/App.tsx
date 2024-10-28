0abaae72f42996cab676a2a75e58c280d0480482
justin-0 <itsjustinoconnell@gmail.com>
2024-10-28 15:28:07 +0000

    hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header";
+import Hero from "./components/hero";
 import "./index.css";
 
 export default function App() {
   return (
     <main className="overflow-hidden antialiased">
       <Header />
+      <Hero />
     </main>
   );
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
+      hero
+    </section>
+  );
+}
